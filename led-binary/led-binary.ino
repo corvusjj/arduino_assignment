4101536b1a767e5cebfe3e861a7ab29a9a7e61bd
John Corbet <mjohncorbet@gmail.com>
2025-06-04 16:45:55 +0800

    change getBinary to setting setBit4Array, make bitArray and other variables as global

--- led-binary/led-binary.ino
+++ led-binary/led-binary.ino
@@ -1,12 +1,19 @@
-// 4bit binary
-void getBinary(int inputNum) {
-  int valueArray[4] = {8, 4, 2, 1};
-  int valueArrayLength = sizeof(valueArray) / sizeof(valueArray[0]);
-  int bit4ValueMax = 15; 
-  int bit4Array[4] = {0, 0, 0, 0};
+int valueArray[4] = {8, 4, 2, 1};
+int valueArrayLength = sizeof(valueArray) / sizeof(valueArray[0]);
+int bit4ValueMax = 15;
 
+int bit4Array[4] = {0, 0, 0, 0};
+
+void resetBit4Array() {
+  for (int i=0; i < valueArrayLength; i++) {
+    bit4Array[i] = 0;
+  }
+}
+
+void setBit4Array(int inputNum) {
+  resetBit4Array();
   for (int i = 0; i < valueArrayLength; i++) {
-    if (inputNum > bit4ValueMax) return bit4Array;
+    if (inputNum > bit4ValueMax) break;
 
     if (inputNum >= valueArray[i]) {
       bit4Array[i] = 1;
@@ -15,16 +22,17 @@ void getBinary(int inputNum) {
 
     if (inputNum <= 0) break;
   }
-
-  Serial.println(bit4Array[0]);
-  Serial.println(bit4Array[1]);
-  Serial.println(bit4Array[2]);
-  Serial.println(bit4Array[3]);
 }
 
 void setup() {
   Serial.begin(9600);
-  getBinary(5);
+
+  setBit4Array(1);
+  Serial.println(bit4Array[0]);
+  Serial.println(bit4Array[1]);
+  Serial.println(bit4Array[2]);
+  Serial.println(bit4Array[3]);
+
 }
 
 void loop() {}
