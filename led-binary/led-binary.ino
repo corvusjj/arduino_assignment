a2c3236b4c41ca785bca737055d7bb76d2d1df98
John Corbet <mjohncorbet@gmail.com>
2025-06-05 17:51:46 +0800

    add buzzer as binary count

--- led-binary/led-binary.ino
+++ led-binary/led-binary.ino
@@ -6,6 +6,7 @@ const int ledPin0 = 9;
 const int ledPin1 = 10;
 const int ledPin2 = 11;
 const int ledPin3 = 12;
+const int buzzerPin = 13;
 
 int bit4Array[4] = {0, 0, 0, 0};
 
@@ -36,11 +37,16 @@ void setBinaryLedPins() {
   digitalWrite(ledPin3, bit4Array[3]);
 }
 
-void runLedBinaryCount() {
+void runLedAndBuzzerBinaryCount() {
   for (int i = 0; i <= bit4ValueMax; i++) {
     setBit4Array(i);
     setBinaryLedPins();
-    delay(1000);
+
+    digitalWrite(buzzerPin, HIGH);
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+
+    delay(900);
   }
 }
 
@@ -50,8 +56,9 @@ void setup() {
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-	runLedBinaryCount();
+	runLedAndBuzzerBinaryCount();
 }
