4f57cc20474fd401487bf00059fc9823fff02093
John Corbet <mjohncorbet@gmail.com>
2025-07-01 12:51:17 +0800

    setup ledpins

--- potentiometer-led-scale/potentiometer-led-scale.ino
+++ potentiometer-led-scale/potentiometer-led-scale.ino
@@ -1,4 +1,7 @@
 const int potenPin = A0;
+const int ledPins[6] = {12, 11, 10, 9, 8, 7};
+const int ledPinsLength = sizeof(ledPins) / sizeof(ledPins[0]);
+
 const int delayT = 1000;
 
 float getVoltage2(int analogVal) {
@@ -7,6 +10,9 @@ float getVoltage2(int analogVal) {
 
 void setup() {
   pinMode(potenPin, INPUT);
+  for (int i = 0; i < ledPinsLength; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 
   Serial.begin(9600);
 };
@@ -15,6 +21,10 @@ void loop () {
   const int potenValue = analogRead(potenPin);
   const float voltage = getVoltage2(potenValue);
   Serial.println(voltage);
-  
-  delay(delayT);
+
+  digitalWrite(ledPins[1], HIGH);
+  digitalWrite(ledPins[2], HIGH);
+  digitalWrite(ledPins[3], HIGH);
+
+  // delay(delayT);
 };
