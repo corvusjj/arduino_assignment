171c94b677db032bb664c063d1de5411f8beedb9
John Corbet <mjohncorbet@gmail.com>
2025-07-01 13:34:35 +0800

    activate led scale based on reference voltage

--- potentiometer-led-scale/potentiometer-led-scale.ino
+++ potentiometer-led-scale/potentiometer-led-scale.ino
@@ -2,29 +2,29 @@ const int potenPin = A0;
 const int ledPins[6] = {12, 11, 10, 9, 8, 7};
 const int ledPinsLength = sizeof(ledPins) / sizeof(ledPins[0]);
 
-const int delayT = 1000;
-
 float getVoltage2(int analogVal) {
   return (5.0/1023.0) * analogVal;
 }
 
+void activateLedScale(float voltage) {
+  const float maxLedIndex = (voltage/5) * ledPinsLength;
+  // maxLedIndex will be 1.00 to 6.00 as from ledPinsLength ratio to 0-5 voltage
+
+  for(int i = 0; i < ledPinsLength; i++) {
+    digitalWrite(ledPins[i], (maxLedIndex - 1 >= i)? HIGH: LOW);
+    // -1 to compenstae for starting 0, >= to reach 5.0 as the highest index
+  }
+}
+
 void setup() {
   pinMode(potenPin, INPUT);
-  for (int i = 0; i < ledPinsLength; i++) {
+  for(int i = 0; i < ledPinsLength; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
-
-  Serial.begin(9600);
 };
 
 void loop () {
   const int potenValue = analogRead(potenPin);
   const float voltage = getVoltage2(potenValue);
-  Serial.println(voltage);
-
-  digitalWrite(ledPins[1], HIGH);
-  digitalWrite(ledPins[2], HIGH);
-  digitalWrite(ledPins[3], HIGH);
-
-  // delay(delayT);
+  activateLedScale(voltage);
 };
